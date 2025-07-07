756e139a9a4b20454bfd1fe3abc15f0987fc74e8
v.bogdanov <vladimir.bogdanov.usa@gmail.com>
2025-07-07 16:37:40 -0400

    Add testing libraries: MockK and coroutines-test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,9 +54,11 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+    testImplementation(libs.mockk)
+    testImplementation(libs.coroutines.test)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    
+
     implementation(libs.retrofit)
     implementation(libs.retrofit.moshi)
     implementation(libs.moshi)
